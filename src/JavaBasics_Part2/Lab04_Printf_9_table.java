cb9080f523837cc66d870bb52eda591dd33914cb
tajuddin.allakhan <taj793@rediffmail.com>
2025-01-12 12:00:51 +0530

    Printf Assignment 11th Jan

---
+++ src/JavaBasics_Part2/Lab04_Printf_9_table.java
@@ -0,0 +1,29 @@
+package JavaBasics_Part2;
+
+public class Lab04_Printf_9_table {
+    public static void main(String[] args) {
+        int b=9;
+        System.out.println();
+        System.out.printf(" 9 X 1 = %d", b);
+        System.out.println();
+        System.out.printf(" 9 X 2 = %d", b*2);
+        System.out.println();
+        System.out.printf(" 9 X 3 = %d", b*3);
+        System.out.println();
+        System.out.printf(" 9 X 4 = %d", b*4);
+        System.out.println();
+        System.out.printf(" 9 X 5 = %d", b*5);
+        System.out.println();
+        System.out.printf(" 9 X 6 = %d", b*6);
+        System.out.println();
+        System.out.printf(" 9 X 7 = %d", b*7);
+        System.out.println();
+        System.out.printf(" 9 X 8 = %d", b*8);
+        System.out.println();
+        System.out.printf(" 9 X 9 = %d", b*9);
+        System.out.println();
+        System.out.printf(" 9 X 10 = %d", b*10);
+        System.out.println();
+
+    }
+}
